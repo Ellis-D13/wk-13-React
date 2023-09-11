45aae3a0a216f486e433e888221f8cd5a1702535
Ellis-D13 <135081698+Ellis-D13@users.noreply.github.com>
2023-09-11 16:47:33 -0500

    updated files

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'; 
-import { ReactDOM } from 'react';
-
-let element= React.createElement('h1'), {}, ('this is react!');
-
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import React from "react";
+import { ReactDOM } from "react";
+
+let element=React.createElement('h1', {}, 'This is react!');
