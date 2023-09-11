d5786952e56a162028ba4650285fea1dbf0758fb
Ellis-D13 <135081698+Ellis-D13@users.noreply.github.com>
2023-09-11 16:54:54 -0500

    updated src folder with index.html and index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import React from "react";
 import { ReactDOM } from "react";
 
 let element=React.createElement('h1', {}, 'This is react!');
+ReactDOM.render(element, document.getElementById ('app'));
